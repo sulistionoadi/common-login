0d1a40ae4c1bae0b2289ffea56a5a001d2785260
Adi Sulistiono <sulistiono.adi@gmail.com>
2020-08-23 23:26:45 +0700

    enhancement dao

--- src/main/java/com/sulistionoadi/ngoprek/common/login/service/MasterUserService.java
+++ src/main/java/com/sulistionoadi/ngoprek/common/login/service/MasterUserService.java
@@ -5,20 +5,22 @@ import java.util.Optional;
 
 import com.sulistionoadi.ngoprek.common.dto.StatusActive;
 import com.sulistionoadi.ngoprek.common.dto.security.MasterUserDTO;
+import com.sulistionoadi.ngoprek.common.exception.CommonException;
 import com.sulistionoadi.ngoprek.common.pss.dto.PssFilter;
 
 public interface MasterUserService {
 
-	public void save(MasterUserDTO dto) throws Exception;
-	public void update(MasterUserDTO dto) throws Exception;
-	public Optional<MasterUserDTO> findOne(Long id) throws Exception;
-	public Optional<MasterUserDTO> findOneFetchEager(Long id) throws Exception;
-	public Optional<MasterUserDTO> findByUsername(String username) throws Exception;
-	public Optional<MasterUserDTO> findByUsernameFetchEager(String username) throws Exception;
-	public Long count(PssFilter filter, StatusActive statusActive) throws Exception;
-	public List<MasterUserDTO> filter(PssFilter filter, StatusActive statusActive) throws Exception;
-	public List<MasterUserDTO> filterFetchEager(PssFilter filter, StatusActive statusActive) throws Exception;
-	public void delete(Long id) throws Exception;
-	public void setActive(Long id, Boolean bool) throws Exception;
+	public void save(MasterUserDTO dto) throws CommonException;
+	public void update(MasterUserDTO dto) throws CommonException;
+	public Optional<MasterUserDTO> findOne(Long id) throws CommonException;
+	public Optional<MasterUserDTO> findOneFetchEager(Long id) throws CommonException;
+	public Optional<MasterUserDTO> findByUsername(String username) throws CommonException;
+	public Optional<MasterUserDTO> findByUsernameFetchEager(String username) throws CommonException;
+	public Long count(PssFilter filter, StatusActive statusActive) throws CommonException;
+	public List<MasterUserDTO> filter(PssFilter filter, StatusActive statusActive) throws CommonException;
+	public List<MasterUserDTO> filterFetchEager(PssFilter filter, StatusActive statusActive) throws CommonException;
+	public void delete(Long id) throws CommonException;
+	public void setAsDelete(Long id, String updatedBy) throws CommonException;
+	public void setActive(Long id, Boolean bool, String updatedBy) throws CommonException;
 	
 }
